fc2bb4f4b9f9846e6cf5ef4659089b96d7565375
Afik Cohen <aphex@aphex.cx>
2016-03-30 18:55:23 -0700

    Set up retrofit2 and woops I used the wrong moshi dependency. DAMN YOU JAKE WHARTON

--- app/src/main/java/cx/aphex/slackteamviewer/activities/MainActivity.java
+++ app/src/main/java/cx/aphex/slackteamviewer/activities/MainActivity.java
@@ -4,12 +4,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import cx.aphex.slackteamviewer.R;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.moshi.MoshiConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Trailing slash is needed
+    public static final String BASE_URL = "https://slack.com/api/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build();
     }
 }
