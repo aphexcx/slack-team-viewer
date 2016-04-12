27f2e08f507c3b665343e549e9c90ee787055ab8
Afik Cohen <aphex@aphex.cx>
2016-04-12 18:41:56 +0200

    Get rid of some old logging, woops

--- app/src/main/java/cx/aphex/slackteamviewer/models/OptionAdapter.java
+++ app/src/main/java/cx/aphex/slackteamviewer/models/OptionAdapter.java
@@ -2,7 +2,6 @@ package cx.aphex.slackteamviewer.models;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 
 import com.squareup.moshi.FromJson;
 import com.squareup.moshi.ToJson;
@@ -12,6 +11,7 @@ import fj.data.Option;
 /**
  * Created by aphex on 4/12/16.
  */
+
 public class OptionAdapter {
     @ToJson
     String toJson(@NonNull Option<String> stringOption) {
@@ -20,9 +20,6 @@ public class OptionAdapter {
 
     @FromJson
     Option<String> fromJson(@Nullable String string) {
-        if (string == null) {
-            Log.wtf("NULL", "string is null!");
-        }
         return Option.fromNull(string);
     }
 }
